9c14a5adc1825cdc192e67fe0e68c9be9651b893
Tuli Jakobson <turbotuli@gmail.com>
2017-06-12 20:57:41 +0000

    Initial file structure & User model

---
+++ db/migrate/20170612203849_create_brews.rb
@@ -0,0 +1,9 @@
+class CreateBrews < ActiveRecord::Migration[5.0]
+  def change
+    create_table :brews do |t|
+      t.string :name, null: false
+      t.string :description
+      t.integer :rating
+    end
+  end
+end
