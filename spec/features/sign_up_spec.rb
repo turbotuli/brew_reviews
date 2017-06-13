de6eb37138f522655836c3c43efebbdca6d4356a
Tuli Jakobson <turbotuli@gmail.com>
2017-06-13 05:05:55 +0000

    user sign up complete

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -28,9 +28,28 @@ feature 'sign up' , %Q{
   end
 
 
-  scenario 'required information is not supplied'
+  scenario 'required information is not supplied' do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign Up'
 
-  scenario 'password confirmation does not match confirmation'
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+  end
+
+  scenario 'password confirmation does not match confirmation' do
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'First Name', with: 'John'
+    fill_in 'Last Name', with: 'Smith'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'differentpassword'
+    click_button 'Sign Up'
+
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
+  end
 
 
 end
